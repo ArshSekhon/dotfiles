99d2d5d9f29697d6b8d594fe4b25deea916d5426
Arsh Sekhon <arshs3khon@gmail.com>
2025-10-15 02:28:30 +0000

    Add tmux config.

---
+++ run_once_tmux_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Only clone TPM if directory doesn't exist
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
